b04d5ea5abd01a93da4ee110fae64811958775f2
halinh122ta <halinh122ta@gmail.com>
2023-08-15 23:04:34 +0700

    start 13

--- untils/db.js
+++ untils/db.js
@@ -1,9 +1,9 @@
 const mysql = require('mysql');
 var dbConn = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'appload'
+    host: 'sql6.freemysqlhosting.net',
+    user: 'sql6639966',
+    password: 'KEhDT2nnJz',
+    database: 'sql6639966'
 });
 dbConn.connect()
 module.exports = {
